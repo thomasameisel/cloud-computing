daa0851b3c1c7798596c221666c604524391cf8d
Lauren Elizabeth Buck <laurenebuck@Laurens-MBP.pavlov.gateway>
2015-09-21 17:39:28 -0500

    First commit

--- prime.py
+++ prime.py
@@ -1,5 +1,5 @@
 
-import math
+import time
 
 #Author: Lauren E Buck
 #Created: September 21, 2015
@@ -16,23 +16,31 @@ def is_prime(num):
     b = True
     c = True
     
+    t1 = time.clock()
+    
     if  num <= 1:
-        print('This is NOT a prime number.')
+        #print('This is NOT a prime number.')
         a = False;
-    elif num <= 3:
-        print('This is a prime number.')
+    #elif num <= 3:
+        #print('This is a prime number.')
     elif num % 2 == 0 or num % 3 == 0:
-        print('This is NOT a prime number.')
+        #print('This is NOT a prime number.')
         b = False;
     i = 5
     while i*i <= num:
         if num % i == 0 or num % (i + 2) == 0:
-            print('This is NOT a prime number.')
+            #print('This is NOT a prime number.')
             c = False;
         i = i + 6
 
     if a != False and b != False and c != False:
         print('This is a prime number.')
+        t2 = time.clock()
+        print('Processing time: ', t2-t1)
+    else:
+        print('This is not a prime number.')
+        t2 = time.clock()
+        print('Processing time:', t2-t1)
 
 #Function call
 is_prime(num)
