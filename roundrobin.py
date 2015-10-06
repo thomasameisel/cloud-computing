cb7a2e8c479b4730a09647d4c3a03fd7fa3c0f33
lbuck92 <lauren.e.buck.1@vanderbilt.edu>
2015-10-06 12:29:23 -0500

    logic ideas

--- roundrobin.py
+++ roundrobin.py
@@ -1,8 +1,7 @@
 
 
-#Round Robin Logic
+#Round Robin + Priority Logic
 #I did not want to place this in the server code and mess it up for now...
-
 
 round_robin = True
 machine_use = 0
@@ -10,6 +9,7 @@ machine_use = 0
 def allocate_server():
 
     global machine_use
+    global round_robin
 
     if len(vm_array) > 1 :
         #Round Robin Method:
@@ -19,8 +19,12 @@ def allocate_server():
             machine_use = 1
         elif round_robin == True and machine_use == 1:
             #code to send a request to the second vm
+            #change machine_use to 0 so the next request goes to the first vm
             machine_use = 0
 
         #Priority Method:
-        if round_robin == False and machine_use == 0:
-            #Need to figure out this one still
+        if round_robin == False:
+            #If the average processing time for the first machine is greater than 1,
+            #start sending all of the requests to the second machine.
+            if average_time > 1:
+            #code to send a request to the second vm
