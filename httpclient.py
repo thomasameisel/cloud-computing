8748e1f72502192fc203053027a5e23679b6bf32
Ubuntu <thomas.meisel95@gmail.com>
2015-10-28 16:14:45 +0000

    change client

--- httpclient.py
+++ httpclient.py
@@ -10,16 +10,23 @@ from random import randint
 def main (num):
 
     params = {'num':num}
-    r = requests.get("http://10.10.1.123:8080", params=params)
+    r = requests.get("http://10.10.1.38:8080", params=params)
     return r.json()
 
 # invoke main
 if __name__ == "__main__":
     for num in range(1, 5):
-	num = 101
-	data = main(num)
-        print "Num: %s Time: %s" % (101, data["processing_time"])
-    while True:
 	num = 101010101101
 	data = main(num)
-        print "Num: %s Time: %s" % (num, data["processing_time"])
+        print "Num: %s Time: %s Average: %s" % (101, data["processing_time"], data["average_processing_time"])
+	time.sleep(45)
+    for num in range(1, 3):
+	num = 1010101011011111
+	data = main(num)
+        print "Num: %s Time: %s Average: %s" % (num, data["processing_time"], data["average_processing_time"])
+	time.sleep(45)
+    for num in range(1, 10):
+	num = 101010101101
+	data = main(num)
+	print "Num: %s Time: %s Average: %s" % (101, data["processing_time"], data["average_processing_time"])
+        time.sleep(45)
